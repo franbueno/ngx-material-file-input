00e34b38dd7d55cf2da94d6c94a742cb31e10ee0
Francisco Bueno <franciscobueno@me.com>
2023-06-16 00:59:36 +0200

    Update to Angular 15

--- apps/demo/src/app/appearance/appearance.component.ts
+++ apps/demo/src/app/appearance/appearance.component.ts
@@ -13,35 +13,9 @@ export class AppearanceComponent implements OnInit {
 
   ngOnInit() {
     this.formDoc = this._fb.group({
-      legacyNoLabel: [],
-      legacy: [],
-      standard: [],
       fill: [],
       outline: []
     });
-  }
-
-  get legacyNoLabel() {
-    return `<mat-form-field appearance="legacy">
-      <ngx-mat-file-input formControlName="legacy" placeholder="Basic legacy placeholder" ></ngx-mat-file-input>
-      <mat-icon matSuffix>folder</mat-icon>
-    </mat-form-field>`;
-  }
-
-  get legacy() {
-    return `<mat-form-field appearance="legacy">
-      <mat-label>Basic legacy input</mat-label>
-      <ngx-mat-file-input formControlName="legacy" placeholder="Basic legacy placeholder" ></ngx-mat-file-input>
-      <mat-icon matSuffix>folder</mat-icon>
-    </mat-form-field>`;
-  }
-
-  get standard() {
-    return `<mat-form-field appearance="standard">
-      <mat-label>Basic standard input</mat-label>
-      <ngx-mat-file-input formControlName="standard" placeholder="Basic standard placeholder" ></ngx-mat-file-input>
-      <mat-icon matSuffix>folder</mat-icon>
-    </mat-form-field>`;
   }
 
   get fill() {
